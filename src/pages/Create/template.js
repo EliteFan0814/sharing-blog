b1dcf277a2067bc526c48c1a1e992531ac68914c
EliteFan <fan552426811@outlook.com>
2019-06-01 20:55:36 +0800

    page

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -14,6 +14,7 @@ export default {
     onCreate() {
       blog.createBlog({ title: this.title, content: this.content, description: this.description, atIndex: this.atIndex})
         .then(res => {
+          // 如果创建成功，使用 elementui 弹出成功提醒
           this.$message.success(res.msg)
           this.$router.push({ path: `/detail/${res.data.id}`})
         })
